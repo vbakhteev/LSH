8c347c37b8f9b14aab82ebe619ad52754197695a
Bakhteev Vladislav <35462234+vbakhteev@users.noreply.github.com>
2022-08-31 13:47:03 +0200

    Update cMinhash.pyx

--- lsh/cMinhash.pyx
+++ lsh/cMinhash.pyx
@@ -93,3 +93,26 @@ def minhash_32(char* c_str, int strlen,
             # reset string pointer for next hash
             c_str -= strlen - char_ngram + 1
     return fingerprint
+
+
+def shingles(str text, int char_ngram = 5):
+    cdef int head
+    cdef set result = set()
+
+    for head in range(0, len(text) - char_ngram):
+        result.add(text[head:head + char_ngram])
+
+    return result
+
+
+def jaccard(str s1, str s2):
+    set1 = shingles(s1)
+    set2 = shingles(s2)
+
+    cdef float jaccard_sim = 0
+    cdef Py_ssize_t length_intersect = 0
+    if len(set1) + len(set2) > 0:
+        length_intersect = len(set1 & set2)
+        jaccard_sim = length_intersect / (len(set1) + len(set2) - length_intersect)
+
+    return jaccard_sim
